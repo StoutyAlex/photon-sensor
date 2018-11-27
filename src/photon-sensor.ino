7fb0105b3532bac5bdc01fea3f62b9730a031d53
StoutyAlex <alexstout009@hotmail.com>
2018-11-27 22:17:00 +0000

    Added ability to post data to a server

--- src/photon-sensor.ino
+++ src/photon-sensor.ino
@@ -2,12 +2,11 @@
 #include "MPU9150.h"
 #include "Si1132.h"
 
-// TCPClient client;
-// const byte SERVER[] = {10,42,72,159};
-// const int PORT = 5000;
+TCPClient client;
+const byte SERVER[] = {192,168,1,211};
+const int PORT = 3000;
 
 long time_reading = 0;
-
 Si70xx tempAndHum;
 Si1132 light;
 
@@ -27,30 +26,31 @@ void loop()
   //Particle.publish("photonSensorData", sensorString, PRIVATE);
   time_reading = Time.now();
   sendMessage(temp, hum, visible);
-  delay(1000);
+  delay(5000);
 }
 
 void sendMessage(float temp, float hum, float visible)
 {
-  // if(client.connect(SERVER, PORT))
-  // {
-  //   if(client.connected())
-  //   {
-  //     String json = "{\"timestamp\": "+ String(time_reading) +", \"temperature\": " + String(temp, 2) + ",\n\"humidity\": " + String(hum, 2) + ",\n\"light\": " + String(visible, 2) + ",\n\"zone\": 1}";
-  //     client.println("POST /smartroom/data/thl HTTP/1.1");
-  //     client.println("Connection: keep-alive");
-  //     client.println("Host: 10.42.72.159");
-  //     client.println("Content-Type: application/json;charset=utf8");
-  //     client.print("Content-Length: ");
-  //     client.println(json.length());
-  //     client.println();
-  //     client.println(json);
-  //     delay(100);
-  //   }
-  //   client.stop();
-  // }
-  // Serial.println("Cannot connect to server");
-  Serial.println(String(temp, 2));
-  Serial.println(String(hum, 2));
-  Serial.println(String(visible, 2));
+  if(client.connect(SERVER, PORT))
+  {
+    if(client.connected())
+    {
+      String json = "{\"timestamp\": "+ String(time_reading) +", \"temperature\": " + String(temp, 2) + ",\n\"humidity\": " + String(hum, 2) + ",\n\"light\": " + String(visible, 2) + ",\n\"sensor_id\": \"" + String(System.deviceID()) + "\"}";
+      Serial.println(json);
+      client.println("POST /api/sensor/data HTTP/1.1");
+      client.println("Connection: keep-alive");
+      client.println("Host: 192.168.1.211");
+      client.println("Content-Type: application/json;charset=utf-8");
+      client.print("Content-Length: ");
+      client.println(json.length());
+      client.println();
+      client.println(json);
+      delay(100);
+    }
+    client.stop();
+  }
+  Serial.println("Cannot connect to server");
+  // Serial.println(String(temp, 2));
+  // Serial.println(String(hum, 2));
+  // Serial.println(String(visible, 2));
 }
